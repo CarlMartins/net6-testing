435d1518652a04bb5f876f343c519b4fc7442869
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-18 19:31:00 +0100

    First test

---
+++ Sparky/SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,21 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+namespace SparkyMSTest;
+
+[TestClass]
+public class CalculatorMSTests
+{
+    [TestMethod]
+    public void AddNumbers_InputTwoInt_GetCorrectAddition()
+    {
+        // Arrange
+        Calculator calculator = new();
+
+        // Act
+        int result = calculator.AddNumbers(10, 20);
+
+        // Assert
+        Assert.AreEqual(30, result);
+    }
+}
