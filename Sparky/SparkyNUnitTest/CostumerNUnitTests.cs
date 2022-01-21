0ef5c15e84a0409009da8d24080aef29dab0e4f5
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-21 19:15:49 -0300

    Global initialization for test class

--- Sparky/SparkyNUnitTest/CostumerNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CostumerNUnitTests.cs
@@ -5,19 +5,25 @@ namespace Sparky;
 [TestFixture]
 public class CostumerNUnitTests
 {
+    private Costumer _costumer;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _costumer = new Costumer();
+    }
+    
     [Test]
     public void CombineName_InputFirstAndLastName_ReturnFullName()
     {
-        var costumer = new Costumer();
-
-        costumer.GreetAndCombineNames("Carlos", "Martins");
+        _costumer.GreetAndCombineNames("Carlos", "Martins");
         
-        Assert.AreEqual("Hello, Carlos Martins!", costumer.GreetMessage);
-        Assert.That(costumer.GreetMessage, Is.EqualTo("Hello, Carlos Martins!"));
-        Assert.That(costumer.GreetMessage, Does.Contain(","));
-        Assert.That(costumer.GreetMessage, Does.StartWith("Hello"));
-        Assert.That(costumer.GreetMessage, Does.EndWith("!"));
-        Assert.That(costumer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+        Assert.AreEqual("Hello, Carlos Martins!", _costumer.GreetMessage);
+        Assert.That(_costumer.GreetMessage, Is.EqualTo("Hello, Carlos Martins!"));
+        Assert.That(_costumer.GreetMessage, Does.Contain(","));
+        Assert.That(_costumer.GreetMessage, Does.StartWith("Hello"));
+        Assert.That(_costumer.GreetMessage, Does.EndWith("!"));
+        Assert.That(_costumer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
     }
 
     [Test]
