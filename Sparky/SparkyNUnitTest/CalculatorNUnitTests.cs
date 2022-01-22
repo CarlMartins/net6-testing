2a0db4e04a0d7be36317112a8937d79824b06a48
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-22 18:32:00 +0100

    Collection helper methods

--- Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -83,5 +83,13 @@ public class CalculatorNUnitTests
         List<int> result = calculator.GetOddRange(5, 10);
         
         Assert.That(result, Is.EquivalentTo(expectedOddRange));
+        Assert.AreEqual(expectedOddRange, result);
+        Assert.Contains(7, result);
+        Assert.That(result, Does.Contain(7));
+        Assert.That(result, Is.Not.Empty);
+        Assert.That(result.Count, Is.EqualTo(3));
+        Assert.That(result, Has.No.Member(6));
+        Assert.That(result, Is.Ordered.Ascending);
+        Assert.That(result, Is.Unique); 
     }
 }
