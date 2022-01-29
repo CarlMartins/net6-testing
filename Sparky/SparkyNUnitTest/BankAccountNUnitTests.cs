69cd5a94165443afd450a39dea94dca2f1f3e26d
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-29 18:45:00 +0100

    Integration test demo

---
+++ Sparky/SparkyNUnitTest/BankAccountNUnitTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace Sparky;
+
+[TestFixture]
+public class BankAccountNUnitTests
+{
+    private BankAccount _bankAccount;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _bankAccount = new (new LogBook());
+    }
+
+    [Test]
+    public void BankDeposit_Add100_ShouldReturnTrue()
+    {
+        var result = _bankAccount.Deposit(100);
+        
+        Assert.IsTrue(result);
+        Assert.That(_bankAccount.Balance, Is.EqualTo(100));
+    }
+}
