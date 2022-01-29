47a3e5189e441df1785528e93419cb031ecbdf30
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-01-29 18:38:00 +0100

    Setup Logger class

--- Sparky/Sparky/BankAccount.cs
+++ Sparky/Sparky/BankAccount.cs
@@ -2,15 +2,18 @@ namespace Sparky;
 
 public class BankAccount
 {
+    private readonly ILogBook _logBook;
     public int Balance { get; set; }
 
-    public BankAccount()
+    public BankAccount(ILogBook logBook)
     {
-        
+        _logBook = logBook;
+        Balance = 0;
     }
 
     public bool Deposit(int amount)
     {
+        _logBook.Message("Deposit Invoked");
         Balance += amount;
         return true;
     }
