3c1e639caa11a252cc7a93813f549b6153c0b458
CarlMartins <lemos_oliveiraa@hotmail.com>
2022-02-20 12:25:52 -0300

    Data access unit tests

--- Sparky/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
+++ Sparky/Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Bongo.DataAccess;
 using Bongo.DataAccess.Repository;
 using Bongo.Models.Model;
@@ -11,6 +12,7 @@ namespace Mongo.DataAccess
     {
         private StudyRoomBooking _studyRoomBookingOne;
         private StudyRoomBooking _studyRoomBookingTwo;
+        private DbContextOptions<ApplicationDbContext> _options;
 
         public StudyRoomBookingRepositoryTests()
         {
@@ -35,23 +37,26 @@ namespace Mongo.DataAccess
             };
         }
 
+        [SetUp]
+        public void Setup()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_Bongo")
+                .Options;
+        }
+
         [Test]
         public void SaveBooking_BookingOne_CheckTheValuesFromDatabase()
         {
-            // Arrange
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "temp_Bongo")
-                .Options;
-            
             // Act
-            using (var context = new ApplicationDbContext(options))
+            using (var context = new ApplicationDbContext(_options))
             {
                 var repository = new StudyRoomBookingRepository(context);
                 repository.Book(_studyRoomBookingOne);
             }
             
             // Assert
-            using (var context = new ApplicationDbContext(options))
+            using (var context = new ApplicationDbContext(_options))
             {
                 var bookingFromDb = context.StudyRoomBookings.FirstOrDefault(u => u.BookingId == 11);
                 Assert.AreEqual(_studyRoomBookingOne.BookingId, bookingFromDb.BookingId);
@@ -59,6 +64,48 @@ namespace Mongo.DataAccess
                 Assert.AreEqual(_studyRoomBookingOne.LastName, bookingFromDb.LastName);
                 Assert.AreEqual(_studyRoomBookingOne.Email, bookingFromDb.Email);
                 Assert.AreEqual(_studyRoomBookingOne.Date, bookingFromDb.Date);
+            }
+        }
+
+        [Test]
+        public void SaveBooking_BookingOneAndTwo_CheckBothTheBookingFromDatabase()
+        {
+            // Arrange
+            var expectedResult = new List<StudyRoomBooking> {_studyRoomBookingOne, _studyRoomBookingTwo};
+
+            using (var context = new ApplicationDbContext(_options))
+            {
+                context.Database.EnsureDeleted();
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(_studyRoomBookingOne);
+                repository.Book(_studyRoomBookingTwo);
+            }
+
+            // Act
+            List<StudyRoomBooking> actualList;
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                actualList = repository.GetAll(null).ToList();
+            }
+            
+            // Assert
+            CollectionAssert.AreEqual(expectedResult, actualList, new BookingCompare());
+        }
+
+        private class BookingCompare : IComparer
+        {
+            public int Compare(object? x, object? y)
+            {
+                var booking1 = (StudyRoomBooking) x;
+                var booking2 = (StudyRoomBooking) y;
+
+                if (booking1.BookingId != booking2.BookingId)
+                {
+                    return 1;
+                }
+
+                return 0;
             }
         }
     }
